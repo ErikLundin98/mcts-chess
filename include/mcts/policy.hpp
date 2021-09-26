9afa9426d3a389d4b5122083196987355180db8d
Erik Lundin <erilu186@student.liu.se>
2021-09-26 11:54:00 +0200

    wrap policies in namespace policy

--- include/mcts/policy.hpp
+++ include/mcts/policy.hpp
@@ -5,38 +5,41 @@
 #include <chess/chess.hpp>
 #include <iostream>
 
-template<int N_ITER>
-inline double random_rollout(chess::position state, chess::side player_turn, std::mt19937 & generator)
+namespace policy
 {
-    double accumulated_t{0};
-    for (int i = 0; i < N_ITER; ++i)
+    template<int N_ITER>
+    double random_rollout(chess::position state, chess::side player_turn, std::mt19937 & generator)
     {
-        chess::position rollout_state = state;
-        // by default, do random rollouts
-        short uneventful_timer = 0;
-        while (!rollout_state.is_checkmate() && !rollout_state.is_stalemate() && uneventful_timer < 50)
+        double accumulated_t{0};
+        for (int i = 0; i < N_ITER; ++i)
         {
-            std::vector<chess::move> available_moves{rollout_state.moves()};
-            chess::move random_choice = *random_element(std::begin(available_moves), std::end(available_moves), generator);
-            chess::undo undo = rollout_state.make_move(random_choice);
-            uneventful_timer = undo.capture != chess::piece::piece_none ? 0 : uneventful_timer + 1;
-        }
+            chess::position rollout_state = state;
+            // by default, do random rollouts
+            short uneventful_timer = 0;
+            while (!rollout_state.is_checkmate() && !rollout_state.is_stalemate() && uneventful_timer < 50)
+            {
+                std::vector<chess::move> available_moves{rollout_state.moves()};
+                chess::move random_choice = *random_element(std::begin(available_moves), std::end(available_moves), generator);
+                chess::undo undo = rollout_state.make_move(random_choice);
+                uneventful_timer = undo.capture != chess::piece::piece_none ? 0 : uneventful_timer + 1;
+            }
 
-        bool is_player_turn = rollout_state.get_turn() == player_turn;
-        if (rollout_state.is_checkmate())
-        {
-            accumulated_t += is_player_turn ? -node::Node::WIN_SCORE : node::Node::WIN_SCORE;
+            bool is_player_turn = rollout_state.get_turn() == player_turn;
+            if (rollout_state.is_checkmate())
+            {
+                accumulated_t += is_player_turn ? -node::Node::WIN_SCORE : node::Node::WIN_SCORE;
+            }
+            else
+            {
+                accumulated_t += node::Node::DRAW_SCORE;
+            }
         }
-        else
-        {
-            accumulated_t += node::Node::DRAW_SCORE;
-        }
-    }
-    
-    return accumulated_t / N_ITER;
-};
+        
+        return accumulated_t / N_ITER;
+    };
 
-inline double bad_rollout(chess::position state, chess::side player_turn)
-{
-    return 0;
+    double bad_rollout(chess::position state, chess::side player_turn)
+    {
+        return 0;
+    };
 }
